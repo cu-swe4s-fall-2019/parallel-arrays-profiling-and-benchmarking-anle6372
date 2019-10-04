ba9272dfbe5991aa8bb2367604e4322a4e371856
Andrew Levitt <anle6372@colorado.edu>
2019-10-03 20:55:36 -0600

    added bash testing file

---
+++ plot_gtex_test.sh
@@ -0,0 +1,11 @@
+conda install pycodestyle
+
+test -e ssshtest || wget -q https://raw.githubusercontent.com/ryanlayer/ssshtest/master/ssshtest
+. ssshtest
+
+run python plot_gtex_cl.py --gene_reads GTEx_Analysis_2017-06-05_v8_RNASeQCv1.1.9_gene_reads.acmg_59.gct.gz --sample_attributes GTEx_Analysis_v8_Annotations_SampleAttributesDS.txt --gene ACTA2 --group_type SMTS --output_file ACTA2.png
+assert_no_stdout
+
+run pycodestyle plot_gtex_cl.py
+assert_no_stdout
+assert_no_stderr
