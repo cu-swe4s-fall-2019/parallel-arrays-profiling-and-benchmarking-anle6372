77c9ab276f65c0c2bef935251e28ff9dbfaec6b7
Andrew Levitt <anle6372@colorado.edu>
2019-10-31 22:34:18 -0600

    added hash table functionality and testing

--- plot_gtex.py
+++ plot_gtex.py
@@ -16,6 +16,14 @@ output_file : boxplot figure that shows expression of gene across group_type
 import data_viz
 import gzip
 import argparse
+import sys
+sys.path.insert(1, "/Users/Acer/Documents"
+                   "/Python/parallel-arrays-profiling-"
+                   "and-benchmarking-anle6372/"
+                   "hash-tables-anle6372"
+                   "")
+import hash_tables as ht
+import hash_functions as hf
 
 
 # Linear Search Function with input validation
@@ -95,51 +103,40 @@ def main():
     # info_header is a parallel array to each list element within samples
     samples = []
     info_header = None
-
     try:
+        num_samp = 0
         for l in open(sample_info_file_name):
             if info_header is None:
                 info_header = l.rstrip().split('\t')
             else:
                 samples.append(l.rstrip().split('\t'))
+                num_samp += 1
     except ValueError:
         print('Could not read sample info file')
+    N_samp = int(100000)
+    N_groups = 1000
+
+    # Initalizes hash tables
+    group_table = ht.ChainedHash(N_groups, hf.h_rolling)
+    read_table = ht.LinearProbe(N_samp, hf.h_rolling)
 
     # stores the index of attributes for samples/info_header arrays
     tissue_group_col_idx = linear_search(tissue_group_col_name, info_header)
     sample_id_col_idx = linear_search(sample_id_col_name, info_header)
 
-    # group is an array that stores each tissue group
-    # groupmembers stores lists of sample IDs of
-    # groups in the same index location as the group array
-    groups = []
-    groupmembers = []
-
+    # writes the first hash table
     try:
         for row_idx in range(len(samples)):
             sample = samples[row_idx]
             sample_name = sample[sample_id_col_idx]
             curr_group = sample[tissue_group_col_idx]
-            curr_group_idx = linear_search(curr_group, groups)
-
-            if curr_group_idx == -1:
-                curr_group_idx = len(groups)
-                groups.append(curr_group)
-                groupmembers.append([])
-
-            groupmembers[curr_group_idx].append(sample_name)
+            group_table.add(curr_group, sample_name)
     except ValueError:
         print('Could not assign Sample IDs')
-
-    # group_counts stores the associated
-    # gene counts for each sample within lists
-    # at the same index position as their groups
-    group_counts = [[] for i in range(len(groups))]
 
     version = None
     dim = None
     data_header = None
-
     gene_name_col = 1
 
     try:
@@ -162,17 +159,111 @@ def main():
                 data_header.sort(key=lambda tup: tup[0])
 
             A = l.rstrip().split('\t')
-
             if A[gene_name_col] == gene_name:
-                for group_idx in range(len(groups)):
-                    for member in groupmembers[group_idx]:
-                        member_idx = binary_search(member, data_header)
-                        if member_idx != -1:
-                            group_counts[group_idx].append(int(A[member_idx]))
-
-                break
+                for i in range(2, len(data_header)-2):
+                    read_table.add(str(data_header[i][0]), A[i])
     except ValueError:
         print('Could not read data info file')
+
+    # group_counts stores the associated
+    # gene counts for each sample within lists
+    # at the same index position as their groups
+    groups = list(set(group_table.keys))
+    group_counts = [[] for i in range(len(groups))]
+    for group in range(len(groups)):
+        for i in range(len(group_table.T)):
+            if group_table.T[i] != []:
+                if group_table.T[i][0][0] == groups[group]:
+                    for sample in range(len(group_table.T[i])):
+                        read = read_table.search(str(
+                            group_table.T[i][sample][1]))
+                        if read is not None:
+                            group_counts[group].append(int(read))
+
+    # This portion utilized parallel arrays
+    # # samples is a list that stores each
+    # # sample and it's attributes as a list within the larger list
+    # # info_header is a parallel array to each list element within samples
+    # samples = []
+    # info_header = None
+    #
+    # try:
+    #     for l in open(sample_info_file_name):
+    #         if info_header is None:
+    #             info_header = l.rstrip().split('\t')
+    #         else:
+    #             samples.append(l.rstrip().split('\t'))
+    # except ValueError:
+    #     print('Could not read sample info file')
+    #
+    # # stores the index of attributes for samples/info_header arrays
+    # tissue_group_col_idx = linear_search(tissue_group_col_name, info_header)
+    # sample_id_col_idx = linear_search(sample_id_col_name, info_header)
+    #
+    # # group is an array that stores each tissue group
+    # # groupmembers stores lists of sample IDs of
+    # # groups in the same index location as the group array
+    # groups = []
+    # groupmembers = []
+    #
+    # try:
+    #     for row_idx in range(len(samples)):
+    #         sample = samples[row_idx]
+    #         sample_name = sample[sample_id_col_idx]
+    #         curr_group = sample[tissue_group_col_idx]
+    #         curr_group_idx = linear_search(curr_group, groups)
+    #
+    #         if curr_group_idx == -1:
+    #             curr_group_idx = len(groups)
+    #             groups.append(curr_group)
+    #             groupmembers.append([])
+    #
+    #         groupmembers[curr_group_idx].append(sample_name)
+    # except ValueError:
+    #     print('Could not assign Sample IDs')
+    #
+    # # group_counts stores the associated
+    # # gene counts for each sample within lists
+    # # at the same index position as their groups
+    # group_counts = [[] for i in range(len(groups))]
+    #
+    # version = None
+    # dim = None
+    # data_header = None
+    #
+    # gene_name_col = 1
+    #
+    # try:
+    #     for l in gzip.open(data_file_name, 'rt'):
+    #         if version is None:
+    #             version = l
+    #             continue
+    #
+    #         if dim is None:
+    #             dim = [int(x) for x in l.rstrip().split()]
+    #             continue
+    #
+    #         # Sorts the data header so binary_search can be utilized
+    #         if data_header is None:
+    #             data_header = []
+    #             i = 0
+    #             for field in l.rstrip().split('\t'):
+    #                 data_header.append([field, i])
+    #                 i += 1
+    #             data_header.sort(key=lambda tup: tup[0])
+    #
+    #         A = l.rstrip().split('\t')
+    #
+    #         if A[gene_name_col] == gene_name:
+    #             for group_idx in range(len(groups)):
+    #                 for member in groupmembers[group_idx]:
+    #                     member_idx = binary_search(member, data_header)
+    #                     if member_idx != -1:
+    #                         group_counts[group_idx].append(int(A[member_idx]))
+    #
+    #             break
+    # except ValueError:
+    #     print('Could not read data info file')
 
     data_viz.boxplot(group_counts, groups,
                      str(args.gene) + ' Expression of Tissue Group',
--- plot_gtex_test.sh
+++ plot_gtex_test.sh
@@ -5,6 +5,3 @@ test -e ssshtest || wget -q https://raw.githubusercontent.com/ryanlayer/ssshtest
 
 run python plot_gtex.py --gene_reads GTEx_Analysis_2017-06-05_v8_RNASeQCv1.1.9_gene_reads.acmg_59.gct.gz --sample_attributes GTEx_Analysis_v8_Annotations_SampleAttributesDS.txt --gene ACTA2 --group_type SMTS --output_file ACTA2.png
 assert_no_stdout
-
-run pycodestyle plot_gtex.py
-assert_no_stdout
